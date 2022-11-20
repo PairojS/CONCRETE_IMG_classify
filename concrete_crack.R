84ed5078f7e6ff39a8037f991352f6328adbdd98
PairojS <s6281423106@pkru.ac.th>
2022-11-20 22:13:25 +0700

    set dimension of image

--- concrete_crack.R
+++ concrete_crack.R
@@ -8,3 +8,7 @@ label_list <- dir("concrete_train_data/")
 output_n <- length(label_list)
 save(label_list, file="label_list.R")
 
+width <- 227
+height<- 227
+target_size <- c(width, height)
+rgb <- 3 #color channels
